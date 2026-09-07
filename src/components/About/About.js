3acefba3b17afe1eee5918267e90573b9874bce0
Jeff Wensley P. Patrimonio <144185506+workjeffwensley@users.noreply.github.com>
2026-09-08 00:13:14 +0800

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -31,7 +31,7 @@ function About() {
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "80px", paddingBottom: "50px" }}
+            style={{ paddingTop: "30px", paddingBottom: "50px" }}
             className="about-img"
           >
             <img src={laptopImg} alt="about" className="img-fluid" />
